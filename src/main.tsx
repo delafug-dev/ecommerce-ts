944ab8053fcc7a4a106fc3fa9ff104286733e87f
Adrián De la Fuente Garceso <adelafuentegarceso@gmail.com>
2024-02-29 16:46:32 +0100

    redux

--- src/main.tsx
+++ src/main.tsx
@@ -8,19 +8,23 @@ import { SearchProvider } from './context/SearchPorivder'
 import { CartProductsProvider } from './context/CartProducts'
 import { ThemeProvider } from './context/ThemeProvider'
 import { ProductoProvider } from './context/ProductoProvider'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <UserAuthProvider>
-        <ProductoProvider>
-          <SearchProvider>
-            <CartProductsProvider>
-              <RouterProvider router={routes}/>
-            </CartProductsProvider>
-          </SearchProvider>
-        </ProductoProvider>
-      </UserAuthProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <UserAuthProvider>
+          <ProductoProvider>
+            <SearchProvider>
+              <CartProductsProvider>
+                <RouterProvider router={routes}/>
+              </CartProductsProvider>
+            </SearchProvider>
+          </ProductoProvider>
+        </UserAuthProvider>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
